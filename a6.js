ac468806aad0476903a27d58f7209f3f21737adc
Programação em Python 2024.4 <36129532024.4@senacrjedu.local>
2024-07-19 11:55:21 -0300

    outo commit

---
+++ a6.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+let i = 0
+// loop infinito até break
+var clicou = 1
+while (true) {
+    if (clicou == 1) {
+        console.log('saindo do loop')
+        break
+        f
+    }
+}
+
+
+while (i <= 10) {
+    console.log(i)
+    i++
+    // i=1 (i=i+1) em python
+
+}
+i = 10
+while (i >= 10) {
+    console.log(i)
+    i--
+    // i-=i (i= i-1) em python
+}
+
+
+
+// como fazer dowhile
+
+
+
